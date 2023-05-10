3ff69609f60ce595e6045a4fa916d2725df41843
Magdalena <magdawnowicka@gmail.com>
2023-05-10 10:01:05 +0200

    :tada: Initialize project

---
+++ book-my-look/src/main/java/ztw/bookmylook/BookMyLookApplication.java
@@ -0,0 +1,13 @@
+package ztw.bookmylook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyLookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyLookApplication.class, args);
+    }
+
+}
