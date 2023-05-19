c4edd2e3bcab3f877c4f16d916f400dde5cf7b57
zsikorska <sikorska.zuzia@gmail.com>
2023-05-19 19:57:53 +0200

    Create skeleton of the pages, add router

--- book-my-look-frontend/src/App.js
+++ book-my-look-frontend/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import NavigationBar from "./components/NavigationBar";
+import Home from "./pages/Home";
+import Contact   from "./pages/Contact";
+import BookVisit from "./pages/BookVisit";
+import EmployeeLogin from "./pages/EmployeeLogin";
+import OurTeam from "./pages/OurTeam";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <Router>
+        <div className="App">
+            <NavigationBar />
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/out-team" element={<OurTeam />} />
+                    <Route path="/book-visit" element={<BookVisit />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/employee-login" element={<EmployeeLogin />} />
+                </Routes>
+            </div>
+        </div>
+      </Router>
   );
 }
 
---
+++ book-my-look-frontend/src/pages/OurTeam.js
@@ -0,0 +1,9 @@
+const OurTeam = () => {
+    return (
+        <div className="our-team">
+            <h2>OurTeam</h2>
+        </div>
+    );
+}
+
+export default OurTeam;
