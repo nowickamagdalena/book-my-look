f74c70281185ae1eff1cb0b66146e81e505f3e80
zsikorska <sikorska.zuzia@gmail.com>
2023-05-19 19:57:27 +0200

    Create navbar component

---
+++ book-my-look-frontend/src/components/NavigationBar.js
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUserLock} from "@fortawesome/free-solid-svg-icons";
+import "../styles/navigationBar.css";
+
+const NavigationBar = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" fixed="top" className="navbar-color">
+            <Container fluid>
+                <Navbar.Brand style={{ color: 'white' }}>BookMyLook</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/" style={{ color: 'white' }}>Home</Nav.Link>
+                        <Nav.Link href="/out-team" style={{ color: 'white' }}>Our team</Nav.Link>
+                        <Nav.Link href="/book-visit" style={{ color: 'white' }}>Book visit</Nav.Link>
+                        <Nav.Link href="/contact" style={{ color: 'white' }}>Contact</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="/employee-login" style={{ color: 'white' }}>
+                            <FontAwesomeIcon icon={faUserLock} />
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavigationBar;
