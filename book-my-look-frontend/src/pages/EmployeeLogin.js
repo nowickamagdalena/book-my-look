5516e846a856af24bfaa69e939490c43d97e0f66
Magdalena <magdawnowicka@gmail.com>
2023-06-11 18:03:49 +0200

    ✨ Add employee login and logout page

--- book-my-look-frontend/src/pages/EmployeeLogin.js
+++ book-my-look-frontend/src/pages/EmployeeLogin.js
@@ -5,32 +5,39 @@ import getLoggedUser from "../context/auth";
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from "react-router-dom";
 import { useAuth } from '../context/AuthContext';
+import Alert from "react-bootstrap/Alert";
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const EmployeeLogin = () => {
     const [user, setUser] = useState({});
     const { setCurrentUser } = useAuth();
     const navigate = useNavigate();
     const loggedInUser = getLoggedUser();
+    const [showAlert, setShowAlert] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
+
+        if (loggedInUser) {
+            setUser(loggedInUser);
+            document.getElementById("sign-in-button").hidden = true;
+            document.getElementById("sign-in-prompt").hidden = true;
+        }
         /* global google */
         google.accounts.id.initialize({
             client_id: "455697851341-vji54tpi9uqtrqfd06eo59najah4tbk3.apps.googleusercontent.com",
-            callback: handleCallbackResponse
+            callback: handleGoogleCallbackResponse
         })
 
         google.accounts.id.renderButton(
-            document.getElementById("signInDiv"),
-            { theme: "outline", size: "large" }
+            document.getElementById("sign-in-button"),
+            { theme: "filled_black", size: "large" }
         )
-
-        if (loggedInUser) {
-          setUser(loggedInUser);
-          document.getElementById("signInDiv").hidden = false;
-        }
     }, [setCurrentUser]);
 
-    function handleCallbackResponse(response) {
+    function handleGoogleCallbackResponse(response) {
         console.log("Encoded JWT ID token: " + response.credential);
         const id_token = response.credential;
         const decodedUser = jwt_decode(response.credential);
@@ -43,20 +50,22 @@ const EmployeeLogin = () => {
             .then((res) => {
                 const userData = res.data;
                 const userObject = { employeeId: userData.id, firstName: userData.firstName, lastName: userData.lastName, email: userData.email, id_token: id_token };
-                document.getElementById("signInDiv").hidden = true;
                 setUser(userObject);
                 setCurrentUser(JSON.stringify(userObject));
+                setShowAlert(false)
+                setErrorMessage('')
                 navigate("/availability");
             })
             .catch((error) => {
-                //TODO: display error to user
                 console.error(error);
+                setErrorMessage(error.response.data.message)
+                setShowAlert(true)
             })
 
     }
 
     function handleSignOut(response) {
-        document.getElementById("signInDiv").hidden = false;
+        document.getElementById("sign-in-button").hidden = false;
         setUser(null);
         setCurrentUser();
         navigate("/");
@@ -66,15 +75,41 @@ const EmployeeLogin = () => {
 
 
     return (
-        <div className="employee-login">
-            <h2>EmployeeLogin</h2>
-            <div id="signInDiv"></div>
-            {
-                user && <div id="logoutDiv"></div>
-            }
-            {Object.keys(user).length !== 0 &&
-             <Button variant="secondary" onClick={(e) => handleSignOut(e)}>Sign out</Button>
-            }
+        <div className="employee-login page">
+            <div className="employee-panel-container">
+                {loggedInUser && <div id="sign-out-div"><Button id="sign-out-button" className="float-right" variant="secondary" onClick={(e) => handleSignOut(e)}>Sign out</Button></div>}
+                <div className="employee-panel">
+                    <h2>Employee panel</h2>
+                    <div id="sign-in-prompt"><p>Click the button below to sign in with your Google account</p></div>
+                    <div id="sign-in-div">
+                        <div id="sign-in-button"></div>
+                    </div>
+
+                    {loggedInUser && <div className="employee-info">
+                        <Form>
+                            <Form.Group className="mb-3">
+                                <Form.Label column >First name</Form.Label>
+                                <Form.Control column type="text" readOnly
+                                    value={loggedInUser.firstName} />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Last name</Form.Label>
+                                <Form.Control type="text" readOnly
+                                    value={loggedInUser.lastName} />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="text" readOnly
+                                    value={loggedInUser.email} />
+                            </Form.Group>
+                        </Form>
+                    </div>}
+                </div>
+            </div>
+            <Alert show={showAlert} variant="danger">
+                <Alert.Heading>Something went wrong!</Alert.Heading>
+                {errorMessage}
+            </Alert>
         </div>
     );
 }
