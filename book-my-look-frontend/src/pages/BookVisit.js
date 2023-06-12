bf4c776d93cdd78a1cf834ddba5e53caa12f785f
Magdalena <magdawnowicka@gmail.com>
2023-06-12 14:53:32 +0200

    🚧 fix brackets

--- book-my-look-frontend/src/pages/BookVisit.js
+++ book-my-look-frontend/src/pages/BookVisit.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import useBeforeRender from '../utils/utils';
 
 const BookVisit = () => {
-    const { services, setServices } = useState([]);
-    const { employees, setEmployees } = useState([]);
-    const { searchCriteria, setSearchCriteria } = useState({servcie: "1"});
+    const [ criteria, setCriteria ] = useState(null);
+    const [ services, setServices ] = useState([]);
+    const [ employees, setEmployees ] = useState([]);
+    
 
     // const updateEvents = (dateRange) => {
     //     axios.get(`http://localhost:8080/employees/${loggedInUser.employeeId}/availabilities?startDate=${dateRange.start}&endDate=${dateRange.end}`, {
@@ -31,6 +31,7 @@ const BookVisit = () => {
         console.log('dupa');
         axios.get(`http://localhost:8080/salonservices`)
             .then(res => {
+                console.log(res.data);
                 setServices(res.data);
             }).catch(error => {
                 console.error('There was an error!' + error);
@@ -41,52 +42,47 @@ const BookVisit = () => {
         console.log("searching");
     }
 
-    useBeforeRender(() => {
-        getServices();
-    }, []);
+    // useBeforeRender(() => {
+    // }, []);
 
     useEffect(() => {
-        // getServices();
-        setSearchCriteria({service: ''});
+        getServices();
+        setCriteria({ service: "1", employee: "1" });
     }, []);
-
 
     return (
         <div className="book-visit">
             <h2>BookVisit</h2>
-            {searchCriteria !== {} && <Form>
-                <Row className="mb-3">
-                    <Form.Group as={Col} className="mb-3" controlId="modalDayControl">
+            <Form>
+                {criteria &&
+                    <Row className="mb-3"> <Form.Group as={Col} className="mb-3" controlId="modalDayControl">
                         <Form.Label>Service</Form.Label>
                         <Form.Select aria-label="Default select example"
-                            value={searchCriteria.service}
-                            onChange={e => setSearchCriteria({ ...searchCriteria, service: e.target.value })}
+                            value={criteria.service}
+                            onChange={e => setCriteria({ ...criteria, service: e.target.value })}
                         >
-                            <option>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                            {/* {
+                            {
                             services.map( service => {
                                 return (<option key={service.id} value={service.name}>{service.name}</option>)
                             })
-                        } */}
+                        }
                         </Form.Select>
                     </Form.Group>
-                    <Form.Group as={Col} className="mb-3" controlId="modalDayControl">
-                        <Form.Label>Employee</Form.Label>
-                        <Form.Select aria-label="Default select example" disabled>
-                            <option>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group as={Col}>
-                        <Button variant="pink" onClick={searchSlots}>Search</Button>
-                    </Form.Group>
-                </Row>
-            </Form>}
+                        <Form.Group as={Col} className="mb-3" controlId="modalDayControl">
+                            <Form.Label>Employee</Form.Label>
+                            <Form.Select aria-label="Default select example" disabled>
+                                <option>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <Button variant="pink" onClick={searchSlots}>Search</Button>
+                        </Form.Group>
+                    </Row>
+                }
+            </Form>
         </div>
     );
 }
